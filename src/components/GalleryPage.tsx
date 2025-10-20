8176e423fbad37b43cad3ba7ec3c5bcf49a163eb
mdia12 <75540841+mdia12@users.noreply.github.com>
2025-10-20 23:21:10 +0200

    Update GalleryPage.tsx

--- src/components/GalleryPage.tsx
+++ src/components/GalleryPage.tsx
@@ -16,7 +16,7 @@ const GalleryPage = () => {
       src: "https://lfbhdmswnbiostcprrke.supabase.co/storage/v1/object/public/photo%20site%20web/platos%20principal/plato%20principale_4.jpg",
       alt: "Latte art profesional",
       altFr: "Latte art professionnel",
-      category: "Platos principale",
+      category: "Platos principales",
       categoryFr: "Boissons"
     },
     {
